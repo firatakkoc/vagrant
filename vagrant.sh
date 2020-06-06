9a953b4c6704ff0db22a5aa1ece33d5de2e4c192
Firat Akkoc <firat.akkoc@Modanisas-MacBook-Pro.local>
2020-06-06 17:55:29 +0300

    update

--- vagrant.sh
+++ vagrant.sh
@@ -8,7 +8,7 @@ sudo apt-get install vagrant -y
 
 echo "################## vagrant done #########################"
 
-cd /Users/firat.akkoc/Documents/git_repo/vagrant/Vagrant_Files
+cd /Users/firat.akkoc/Documents/git_repo/vagrant
 
 vagrant up Vagranfile
 
