aca3e453d3e03b2cb49b9f96f00eb8c52aeb639d
Firat Akkoc <firat.akkoc@Modanisas-MacBook-Pro.local>
2020-06-07 17:49:52 +0300

    mac script

---
+++ mac_vagrant.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+#sudo apt install virtualbox -y
+
+echo "################## virtualbox done  ##########################"
+
+#sudo apt install vagrant -y
+
+echo "################## vagrant done #########################"
+
+cd /Users/firat.akkoc/Documents/git_repo/vagrant
+
+vagrant up
+
+vagrant ssh
+
+
