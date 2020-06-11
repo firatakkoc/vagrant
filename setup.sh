8758c3eb5b623a7bec6666dada11f7f104e0fbe7
Firat Akkoc <firat.akkoc@Modanisas-MacBook-Pro.local>
2020-06-11 15:28:00 +0300

    11.06.2020_setup1

--- setup.sh
+++ setup.sh
@@ -8,10 +8,11 @@ sleep 2
 sudo apt update
 #sudo apt -y upgrade
 sudo apt -y install ntpdate
+sudo apt -y install snapd
 sudo ntpdate pool.ntp.org
 sudo apt -y install ntp
 sudo apt -y install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
-sudo apt -y install python3-pip python3-dev
+sudo apt -y install python3-pip python3-dev python3-venv python3.4-venv 
 sudo apt -y install nginx
 sudo pip3 install virtualenvwrapper
 echo '############################'
@@ -43,7 +44,7 @@ echo '######################'
 sleep 2
 mkdir /home/vagrant/flask_apps
 cd /home/vagrant/flask_apps
-virtualenv app01_env
+python3.4 -m venv app01_env
 source app01_env/bin/activate
 pip install gunicorn flask
 pip install setproctitle
@@ -78,7 +79,7 @@ sleep 2
 sudo cp /home/vagrant/vagrant/nginx/nginx_app01.conf /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/nginx_app01.conf /etc/nginx/sites-enabled
-sudo systemctl restart nginx
+sudo service nginx start
 echo '####################'
 echo 'NGINX SETUP COMPLETE'
 echo '####################'
